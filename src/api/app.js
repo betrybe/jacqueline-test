ec03bcf5ff65876d41cb1c69f2e56ba77fdd1800
Jacqueline Almeida <jacqueline.almeida@solvis.com.br>
2021-08-21 21:55:14 -0300

    Configs iniciais.

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/', require('./users/router'));
 
 module.exports = app;
--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,9 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
+
+//  URI: Mongo
+mongoose.connect('mongodb://localhost:27017/Cookmaster');
