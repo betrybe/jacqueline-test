d034fb778f5c6baaab0795276f21602c40538f41
Jacqueline Almeida <jacqueline.almeida@solvis.com.br>
2021-08-23 20:24:54 -0300

    Atualização de pacotes e configurações.

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const express = require('express');
+const verify_token = require('./users/security/verify-token')
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -15,5 +17,13 @@ app.get('/', (request, response) => {
 // Não remover esse end-point, ele é necessário para o avaliador
 
 app.use('/', require('./users/router'));
+app.use('/', require('./recipes/router'));
+
+// /images é o caminho/end-point da API onde as imagens estarão disponíveis
+// path.join(__dirname, '..', 'uploads') é o caminho da pasta onde o multer deve salvar suas imagens ao realizar o upload
+// a pasta `uploads` está em `./src/uploads` e não deve ser renomeada ou removida (assim como o arquivo `ratinho.jpg`)
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+
+console.log(express.static(path.join(__dirname, '..', 'uploads')))
 
 module.exports = app;
